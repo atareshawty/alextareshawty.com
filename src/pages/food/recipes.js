d4d8fa1f996ae62bb17f0f8a8088a36d78a38b6e
Alex Tareshawty <alex@tareshawty.com>
2019-01-05 01:47:28 -0500

    Adds some simple food post navigation
    This sill need to enhanced after more recipes are added

---
+++ src/pages/food/recipes.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { graphql, Link, StaticQuery } from 'gatsby';
+
+import CookieSvg from '../../assets/images/cookie-bite-solid.svg';
+import Layout from '../../components/layout';
+import styles from './recipes.module.css';
+
+const query = graphql`
+  query {
+    allRecipesJson {
+      edges {
+        node {
+          metadata {
+            path
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+const compareEdges = (a, b) => a.node.metadata.title.localeCompare(b.node.metadata.title);
+
+const renderRecipeIndex = ({ allRecipesJson: { edges } }) => (
+  <Layout>
+    <h3>
+      Here are some of my go to recipes!
+    </h3>
+    <ul className={styles.list}>
+      {edges.sort(compareEdges).map(({ node }) => (
+        <div key={node.metadata.path} className={styles.listItem}>
+          <CookieSvg className={styles.listIcon} />
+          <Link to={`${node.fields.slug}${node.metadata.path}`}>
+            {node.metadata.title}
+          </Link>
+        </div>
+      ))}
+    </ul>
+  </Layout>
+);
+
+export default () => (
+  <StaticQuery
+    query={query}
+    render={renderRecipeIndex}
+  />
+);
