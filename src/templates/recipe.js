b8885d62ef3f705e12d4ec5391fa9bb330171cf3
Alex Tareshawty <alex@tareshawty.com>
2019-01-16 21:02:53 -0500

    Adds image to recipe page
    And re-designs layout to accommodate for the image

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { graphql } from 'gatsby';
 
 import IngredientsList from '../components/ingredients-list';
-import RecipeInstructionsList from '../components/recipe-instructions-list';
 import Layout from '../components/layout';
+import RecipeInstructionsList from '../components/recipe-instructions-list';
 import RecipeTimingInformation from '../components/recipe-timing-information';
 import styles from './recipe.module.css';
 
@@ -28,17 +29,15 @@ const Recipe = (props) => {
   return (
     <Layout>
       <div className={styles.recipe}>
-        <div className={styles.recipeHeaderContainer}>
-          <header className={styles.recipeHeader}>
-            <h1 className={styles.recipeTitle}>{metadata.title}</h1>
-            <h6 className={styles.recipeCredit}>Courtesy of: {renderCreditLink(metadata.credits)}</h6>
-            <h6 className={styles.recipeCredit}>Image will go here</h6>
-          </header>
+        <div className={styles.leftColumn}>
+          <h1 className={styles.recipeTitle}>{metadata.title}</h1>
+          <h6 className={styles.recipeCredit}>Courtesy of: {renderCreditLink(metadata.credits)}</h6>
           <RecipeTimingInformation timingInformation={data.timingInformation} />
+          <IngredientsList className={styles.ingredientsList} ingredients={data.ingredients} />
         </div>
-        <div className={styles.listsContainer}>
-          <IngredientsList className={styles.smallList} ingredients={data.ingredients} />
-          <RecipeInstructionsList className={styles.largeList} instructions={data.instructions} />
+        <div className={styles.rightColumn}>
+          <Img className={styles.image} fluid={props.data.headerImage.childImageSharp.fluid} />
+          <RecipeInstructionsList className={styles.instructionsList} instructions={data.instructions} />
         </div>
       </div>
     </Layout>
@@ -49,23 +48,40 @@ Recipe.propTypes = {
   data: PropTypes.shape({
     recipesJson: PropTypes.shape({
       metadata: PropTypes.shape({
-        date: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
         credits: PropTypes.shape({
           link: PropTypes.string.isRequired,
           name: PropTypes.string.isRequired,
         }),
+        date: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
       }),
       fields: PropTypes.shape({
         data: PropTypes.string.isRequired,
+      }),
+    }),
+    headerImage: PropTypes.shape({
+      childImageSharp: PropTypes.shape({
+        fluid: PropTypes.shape({
+          base64: PropTypes.string,
+          tracedSVG: PropTypes.string,
+          aspectRatio: PropTypes.number,
+          src: PropTypes.string,
+          srcSet: PropTypes.string,
+          srcWebp: PropTypes.string,
+          srcSetWebp: PropTypes.string,
+          sizes: PropTypes.string,
+          originalImg: PropTypes.string,
+          originalName: PropTypes.string,
+        }),
       }),
     }),
   }),
 };
 
 export default Recipe;
+
 export const query = graphql`
-  query($slug: String!) {
+  query($slug: String!, $headerImageFileName: String!) {
     recipesJson(fields: { slug: { eq: $slug } }) {
       metadata {
         date
@@ -77,6 +93,22 @@ export const query = graphql`
       }
       fields {
         data
+      }
+    }
+    headerImage: file(relativePath: { eq: $headerImageFileName }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, maxHeight: 500) {
+          base64
+          tracedSVG
+          aspectRatio
+          src
+          srcSet
+          srcWebp
+          srcSetWebp
+          sizes
+          originalImg
+          originalName
+        }
       }
     }
   }
